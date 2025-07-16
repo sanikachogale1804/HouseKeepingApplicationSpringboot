a25b93b234e0211d0a40870503c70ab25354a2a0
unknown <https://github.com/sanikachogale1804.com>
2025-07-16 17:52:15 +0530

    16-july-2025

--- src/main/java/com/example/Demo/HouseKeppingApplication/Controller/UserController.java
+++ src/main/java/com/example/Demo/HouseKeppingApplication/Controller/UserController.java
@@ -1,6 +1,10 @@
 package com.example.Demo.HouseKeppingApplication.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +13,7 @@ import com.example.Demo.HouseKeppingApplication.Entity.User;
 import com.example.Demo.HouseKeppingApplication.Service.UserService;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 	
 	@Autowired
@@ -24,5 +29,11 @@ public class UserController {
 		System.out.println(user);
 		return service.verify(user);
 	}
-
+	
+	@GetMapping("/login")
+	public ResponseEntity<String> loginNotAllowed() {
+	    return ResponseEntity
+	        .status(HttpStatus.METHOD_NOT_ALLOWED)
+	        .body("❌ GET not allowed on /login. Use POST instead.");
+	}
 }
